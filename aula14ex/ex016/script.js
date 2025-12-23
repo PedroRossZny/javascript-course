a164b01c2c7d280d946761131ab07ed305e2d4b5
Pedro Henrique Rodrigues Ross <pedrohenriquerodriguesross@gmail.com>
2025-12-23 17:37:07 -0300

    resolução dos exercícios

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,1 +1,27 @@
-alert('Olá!')
+function contar() {
+    var i = window.document.querySelector('input#ini')
+    var f = window.document.querySelector('input#fim')
+    var p = window.document.querySelector('input#pas')
+    var r = window.document.querySelector('div#res')
+
+    r.innerHTML = ''
+
+    if (i.value === '' || f.value === '' || p.value === '') {
+        r.innerHTML = 'Impossível contar!'
+        return
+    }
+    
+    i = Number(i.value)
+    f = Number(f.value)
+    p = Number(p.value)
+
+    if (p <= 0) {
+        alert('Passo inválido! Considerando PASSO 1')
+        p = 1
+    }
+
+    for (var c = i; c <= f; c += p) {
+        r.innerHTML += `${c} 👉`
+    }
+    r.innerHTML += '🏁'
+    }
--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,21 @@
-alert('Olá!')
+function tabuada() {
+    var n = window.document.querySelector('input#num')
+    var r = window.document.querySelector('select#res')
+    
+    c = 1
+
+    if (n.value.length == 0) {
+        alert('Por favor, digite um número!')
+        return
+    }
+
+    n = Number(n.value)
+    if (c == 1) {
+        r.innerHTML = `<option>${n} x ${c} = ${n * c}</option>`
+        c++
+    }
+
+    for (c = 2; c <= 10; c++) {
+        r.innerHTML += `<option>${n} x ${c} = ${n * c}</option>`
+    }
+}
